77782e13be9f9f6c78b029d442547d58561e5792
davidcoats <david@getsafetycone.com>
2023-03-02 10:48:23 -0800

    Initial commit.

---
+++ source/R5T.T0163/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0163
+{
+	/// <summary>
+	/// LinkHref and LinkText strongly-typed link value types.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
